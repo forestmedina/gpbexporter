72294f6ca3d5df3af8f9c84ef9c22706c48e6e82
forestmedina <forestmedina@gmail.com>
2013-06-15 00:49:30 -0430

    Agregado exportado de armaduras

--- gpbexporter/gp3d.py
+++ gpbexporter/gp3d.py
@@ -61,7 +61,7 @@ class Node(Reference):
     tipoNodo =0   #enum NodeType
     transforms =[]   #float[16]
     parent_id  =""   #string
-    children   =[]  #Node[]
+    childrens   =[]  #Node[]
     camera     =None    #Camera
     light      =""   #Light
     model      =None   #Model
@@ -69,13 +69,17 @@ class Node(Reference):
     def __init__(self):
         self.tipo=ReferenceType.NODE;
         self.tipoNodo=1;
-        self.transforms=[0]*16	;
+        self.transforms=[0]*16;
         self.camera=None;
         self.model=None;
+        self.childrens=[];
+        self.parent_id=None;
         return;
 
     def writeReference(self,f):
         Reference.writeReference(self,f);
+        for c in self.childrens:
+            c.writeReference(f);
         if not self.camera is None:
             self.camera.writeReference(f);
         if not self.model is None:
@@ -84,6 +88,8 @@ class Node(Reference):
         
     def updateOffset(self,f):
         Reference.updateOffset(self,f);
+        for c in self.childrens:
+            c.updateOffset(f);
         if not self.camera is None:
             self.camera.updateOffset(f);
         if not self.model is None:
@@ -96,6 +102,8 @@ class Node(Reference):
     def writeExtra(self,f):   
         if not self.model is None:
             self.model.writeExtra(f);
+        for c in self.childrens:
+            c.writeExtra(f);
         return;
     
     def writeData(self,f):
@@ -103,11 +111,17 @@ class Node(Reference):
         f.write(struct.pack("<I",self.tipoNodo));
         for t in self.transforms:
             f.write(struct.pack("<f",t));
-        f.write(struct.pack("<I",len(self.parent_id)+1));#mesh
-        f.write(bytearray('#',"ascii"));
-        if len(self.parent_id)>0:
-            f.write(bytearray(self.parent_id,"ascii"));
-        f.write(struct.pack("<I",0)),#hijos longitud de cadena 0
+        if not self.parent_id is None:
+            f.write(struct.pack("<I",len(self.parent_id)+1));#mesh
+            f.write(bytearray('#',"ascii"));
+            if len(self.parent_id)>0:
+                f.write(bytearray(self.parent_id,"ascii"));
+        else:
+            f.write(struct.pack("<I",0));
+        f.write(struct.pack("<I",len(self.childrens))),#hijos longitud de cadena 0
+        if len(self.childrens)>0:
+            for c in self.childrens:
+                c.writeData(f);
         if not self.camera is None:
             self.camera.writeData(f);
         else:
@@ -127,6 +141,7 @@ class Model (Reference):
     materials=[]     #Material[]
     def __init__(self):
         self.tipo=ReferenceType.MODEL;
+        self.meshSkin=None;
         return;
 
     def writeReference(self,f):
@@ -149,7 +164,11 @@ class Model (Reference):
         f.write(bytearray('#',"ascii"));
         if len(self.mesh.reference)>0:
             f.write(bytearray(self.mesh.reference,"ascii"));
-        f.write(struct.pack("B",0));#meshSkin longitud de cadena 0
+        if self.meshSkin is None:
+            f.write(struct.pack("B",0));#hasMeshSkin no
+        else:
+            f.write(struct.pack("B",1));#hasMeshSkin yes
+            self.meshSkin.writeData(f);
         f.write(struct.pack("<I",0));#material longitud de arreglo 0
         return ;
 
@@ -165,12 +184,17 @@ class Mesh(Reference):
 
     def writeData(self,f):
         self.offset=f.tell();
-        f.write(struct.pack("<I",2));#Cantidad de vertexUsage
+        f.write(struct.pack("<I",4));#Cantidad de vertexUsage
         f.write(struct.pack("<I",1));#VextexUsage 1-POSITION
         f.write(struct.pack("<I",3));#VextexSize 3 (x,y,z)
         f.write(struct.pack("<I",2));#VextexUsage 2-NORMAL
         f.write(struct.pack("<I",3));#VextexSize 3 (x,y,z)
-        f.write(struct.pack("<I",len(self.vertices)*24));#Cantidad de Vertices * 3(Position vertice) * 3 (Normal Vertice) Omitting any other vertice information
+        f.write(struct.pack("<I",7));#VextexUsage 7-BLENDINDICES
+        f.write(struct.pack("<I",4));#VextexSize 4 max joints
+        f.write(struct.pack("<I",6));#VextexUsage 6-BLENDWEIGTHS
+        f.write(struct.pack("<I",4));#VextexSize 4 max joints
+        f.write(struct.pack("<I",len(self.vertices)*(3+3+4+4)*4));#Cantidad de Vertices * 3(Position vertice) * 3 (Normal Vertice) Omitting any other vertice information
+
         for v in self.vertices:
             f.write(struct.pack("<f",v.co[0]));#VextexSize 3 (x,y,z)
             f.write(struct.pack("<f",v.co[1]));#VextexSize 3 (x,y,z)
@@ -178,6 +202,25 @@ class Mesh(Reference):
             f.write(struct.pack("<f",v.normal[0]));#VextexSize 3 (x,y,z)
             f.write(struct.pack("<f",v.normal[1]));#VextexSize 3 (x,y,z)
             f.write(struct.pack("<f",v.normal[2]));#VextexSize 3 (x,y,z)
+            ngroups=0;
+            for g in v.groups:
+                f.write(struct.pack("<f",g.group));
+                ngroups+=1;
+                if ngroups>=4:
+                    break;
+            while ngroups<4:
+                f.write(struct.pack("<f",0));#VextexSize 3 (x,y,z)
+                ngroups+=1;
+            ngroups=0;
+            for g in v.groups:
+                f.write(struct.pack("<f",g.weight));
+                ngroups+=1;
+                if ngroups>=4:
+                    break;
+            while ngroups<4:
+                f.write(struct.pack("<f",0));#VextexSize 3 (x,y,z)
+                ngroups+=1;
+            
         #Omit bounding box
         f.write(struct.pack("<f",0));#Omit bounding box
         f.write(struct.pack("<f",0));#Omit bounding box
@@ -202,7 +245,44 @@ class Mesh(Reference):
             f.write(struct.pack("<I",p.vertices[2]));#VextexSize 3 (x,y,z)
         return ;
         
-        
+class MeshSkin(Reference):
+    
+    def __init__(self):
+        self.tipo=ReferenceType.MESHSKIN;
+        self.bindShape=[0]*16	;
+        self.joints=[];
+        self.jointBindPoses=[];
+        self.boundingBox=None;
+        self.boundingSphere=None;
+        return;
+
+    def writeData(self,f):
+        self.offset=f.tell();
+        print("BindShape "+str(len(self.bindShape)));
+        for b in self.bindShape:
+            f.write(struct.pack("<f",b));
+        f.write(struct.pack("<I",len(self.joints)));#joints
+        for j in self.joints:
+            f.write(struct.pack("<I",len(j.reference)+1));#mesh
+            f.write(bytearray('#',"ascii"));
+            f.write(bytearray(j.reference,"ascii"));
+
+        f.write(struct.pack("<I",len(self.joints)*16));#joints
+        for b in self.jointBindPoses:
+            f.write(struct.pack("<f",b));
+        #Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #f.write(struct.pack("<f",0));#Omit bounding box
+        #Omit bounding sphere
+        #f.write(struct.pack("<f",0));#Omit bounding sphere
+        #f.write(struct.pack("<f",0));#Omit bounding sphere
+        #f.write(struct.pack("<f",0));#Omit bounding sphere
+        #f.write(struct.pack("<f",0));#Omit bounding sphere
+        return ;        
 
 class Camera(Reference):
     cameraType =0#byte {perspective|orthographic}
@@ -234,7 +314,8 @@ class ReferenceType:
     NODE=2;
     MODEL=11;
     MESH=34;
-    CAMERA=34;
+    MESHSKIN=36;
+    CAMERA=32;
 
 
 class NodeType:
@@ -250,6 +331,82 @@ class Exporter(bpy.types.Operator, ExportHelper):
     filename_ext    = ".gpb";
     objetos=[];
     
+    def procesarArmature(self,nodeMesh, mesh):
+        bobject=mesh.parent;
+        armature = bobject.data;
+        nodeMesh.model.meshSkin=MeshSkin();
+        node= Node();
+        node.transforms[0]=bobject.matrix_world[0][0];
+        node.transforms[1]=bobject.matrix_world[1][0];
+        node.transforms[2]=bobject.matrix_world[2][0];
+        node.transforms[3]=bobject.matrix_world[3][0];
+        node.transforms[4]=bobject.matrix_world[0][1];
+        node.transforms[5]=bobject.matrix_world[1][1];
+        node.transforms[6]=bobject.matrix_world[2][1];
+        node.transforms[7]=bobject.matrix_world[3][1];
+        node.transforms[8]=bobject.matrix_world[0][2];
+        node.transforms[9]=bobject.matrix_world[1][2];
+        node.transforms[10]=bobject.matrix_world[2][2];
+        node.transforms[11]=bobject.matrix_world[3][2];
+        node.transforms[12]=bobject.matrix_world[0][3];
+        node.transforms[13]=bobject.matrix_world[1][3];
+        node.transforms[14]=bobject.matrix_world[2][3];
+        node.transforms[15]=bobject.matrix_world[3][3];
+        node.tipoNodo=NodeType.JOINT;
+        node.reference=bobject.name;
+        self.objetos.append(node);
+        huesos={};
+        nodeMesh.model.meshSkin.jointBindPoses=[0]*(len(armature.bones)*16);
+        i=0;
+        for b in armature.bones:
+            bone= Node();
+            bone.reference=b.name;
+            nodeMesh.model.meshSkin.joints.append(bone);
+            bone.transforms[0]=b.matrix_local[0][0];
+            bone.transforms[1]=b.matrix_local[1][0];
+            bone.transforms[2]=b.matrix_local[2][0];
+            bone.transforms[3]=b.matrix_local[3][0];
+            bone.transforms[4]=b.matrix_local[0][1];
+            bone.transforms[5]=b.matrix_local[1][1];
+            bone.transforms[6]=b.matrix_local[2][1];
+            bone.transforms[7]=b.matrix_local[3][1];
+            bone.transforms[8]=b.matrix_local[0][2];
+            bone.transforms[9]=b.matrix_local[1][2];
+            bone.transforms[10]=b.matrix_local[2][2];
+            bone.transforms[11]=b.matrix_local[3][2];
+            bone.transforms[12]=b.matrix_local[0][3];
+            bone.transforms[13]=b.matrix_local[1][3];
+            bone.transforms[14]=b.matrix_local[2][3];
+            bone.transforms[15]=b.matrix_local[3][3];
+            nodeMesh.model.meshSkin.jointBindPoses[0+i*16]=b.matrix_local[0][0];
+            nodeMesh.model.meshSkin.jointBindPoses[1+i*16]=b.matrix_local[1][0];
+            nodeMesh.model.meshSkin.jointBindPoses[2+i*16]=b.matrix_local[2][0];
+            nodeMesh.model.meshSkin.jointBindPoses[3+i*16]=b.matrix_local[3][0];
+            nodeMesh.model.meshSkin.jointBindPoses[4+i*16]=b.matrix_local[0][1];
+            nodeMesh.model.meshSkin.jointBindPoses[5+i*16]=b.matrix_local[1][1];
+            nodeMesh.model.meshSkin.jointBindPoses[6+i*16]=b.matrix_local[2][1];
+            nodeMesh.model.meshSkin.jointBindPoses[7+i*16]=b.matrix_local[3][1];
+            nodeMesh.model.meshSkin.jointBindPoses[8+i*16]=b.matrix_local[0][2];
+            nodeMesh.model.meshSkin.jointBindPoses[9+i*16]=b.matrix_local[1][2];
+            nodeMesh.model.meshSkin.jointBindPoses[10+i*16]=b.matrix_local[2][2];
+            nodeMesh.model.meshSkin.jointBindPoses[11+i*16]=b.matrix_local[3][2];
+            nodeMesh.model.meshSkin.jointBindPoses[12+i*16]=b.matrix_local[0][3];
+            nodeMesh.model.meshSkin.jointBindPoses[13+i*16]=b.matrix_local[1][3];
+            nodeMesh.model.meshSkin.jointBindPoses[14+i*16]=b.matrix_local[2][3];
+            nodeMesh.model.meshSkin.jointBindPoses[15+i*16]=b.matrix_local[3][3];
+            bone.tipoNodo=NodeType.JOINT;
+            huesos[bone.reference]=bone;
+            i+=1;
+            if b.parent is None:
+                node.childrens.append(bone);
+          #      bone.parent_id=node.reference;
+                print(bone.parent_id);
+            else:
+                huesos[b.parent].childrens.append(bone);
+
+                bone.parent_id=huesos[b.parent].reference;
+        return node;
+
 
     def procesarMesh(self, bobject):
         mesh = bobject.to_mesh(bpy.context.scene,True,'PREVIEW');
@@ -293,6 +450,9 @@ class Exporter(bpy.types.Operator, ExportHelper):
         node.model.mesh.reference=bobject.name+"mesh";
         node.model.mesh.vertices=mesh.vertices;
         node.model.mesh.parts=mesh.polygons;
+        self.objetos.append(node);
+        if bobject.parent.type=='ARMATURE':
+            self.procesarArmature(node,bobject);
         return node;
         
         
@@ -335,8 +495,7 @@ class Exporter(bpy.types.Operator, ExportHelper):
         self.objetos=[];
         for ob in bpy.data.objects:
             if ob.type == 'MESH':
-                node = self.procesarMesh(ob);
-                self.objetos.append(node);
+                self.procesarMesh(ob);
         # Create a file header object with data stored in the body section   
         # Open the file for writing
         camera = self.procesarCamera(bpy.data.cameras[bpy.context.scene.camera.name]);
@@ -356,8 +515,9 @@ class Exporter(bpy.types.Operator, ExportHelper):
         file.write(struct.pack("<I",len(self.objetos)));
         for o in  self.objetos:
             o.writeNode(file);
-        for o in  self.objetos:
-            o.writeExtra(file);
+
+
+       
 #write data of scene
         file.write(struct.pack("<I",len(camera.reference)+1));#camara xref
         file.write(bytearray('#',"ascii"));
@@ -366,6 +526,8 @@ class Exporter(bpy.types.Operator, ExportHelper):
         file.write(struct.pack("<f",1.0));
         file.write(struct.pack("<f",1.0));
         file.write(struct.pack("<f",1.0));
+        for o in  self.objetos:
+            o.writeExtra(file);
         for o in  self.objetos:
             o.updateOffset(file);
         scene.updateOffset(file);
